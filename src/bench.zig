ee4ad809d513b753e8bd8344abf804228a0af369
Wannes Vantorre <vantorrewannes@gmail.com>
2025-12-31 23:30:55 +0100

    feature: Implement encoder.

--- src/bench.zig
+++ src/bench.zig
@@ -1,6 +1,82 @@
 const std = @import("std");
-const root = @import("skim");
+const zbench = @import("zbench");
+const skim = @import("skim");
+
+fn readFile(allocator: std.mem.Allocator, path: []const u8) ![]u8 {
+    const file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+    const stat = try file.stat();
+    const buffer = try allocator.alloc(u8, stat.size);
+    _ = try file.readAll(buffer);
+    return buffer;
+}
+
+pub fn EncodeBenchmark(Encoder: type) type {
+    return struct {
+        const Self = @This();
+        ctx: *Encoder,
+        input: []const u8,
+        output: []u8,
+
+        fn init(ctx: *Encoder, input: []const u8, output: []u8) Self {
+            return .{ .ctx = ctx, .input = input, .output = output };
+        }
+
+        pub fn run(self: Self, _: std.mem.Allocator) void {
+            _ = self.ctx.compressBlockToBuffer(self.input, self.output);
+        }
+    };
+}
 
 pub fn main() !void {
-    std.debug.print("SKIM bench\n", .{});
+    var arena = std.heap.ArenaAllocator.init(std.heap.smp_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    var stdout = std.fs.File.stdout().writerStreaming(&.{});
+    const writer = &stdout.interface;
+    var bench = zbench.Benchmark.init(allocator, .{});
+    defer bench.deinit();
+
+    var input_data: []u8 = undefined;
+    var output_data: []u8 = undefined;
+
+    if (args.len > 1) {
+        const file_path = args[1];
+        try writer.print("Loading {s}...\n", .{file_path});
+
+        input_data = try readFile(allocator, file_path);
+        defer allocator.free(input_data);
+
+        {
+            const Encoder = skim.FastEncoder;
+            var encoder = try Encoder.init(allocator);
+
+            output_data = try allocator.alloc(u8, Encoder.outputBufferBound(input_data.len));
+            defer allocator.free(output_data);
+
+            const name = try std.fmt.allocPrint(allocator, "FastEncoder: {s}", .{std.fs.path.basename(file_path)});
+            defer allocator.free(name);
+
+            try bench.addParam(name, &EncodeBenchmark(Encoder).init(&encoder, input_data, output_data), .{});
+        }
+
+        {
+            const Encoder = skim.TinyEncoder;
+            var encoder = try Encoder.init(allocator);
+
+            output_data = try allocator.alloc(u8, Encoder.outputBufferBound(input_data.len));
+            defer allocator.free(output_data);
+
+            const name = try std.fmt.allocPrint(allocator, "TinyEncoder: {s}", .{std.fs.path.basename(file_path)});
+            defer allocator.free(name);
+
+            try bench.addParam(name, &EncodeBenchmark(Encoder).init(&encoder, input_data, output_data), .{});
+        }
+    }
+
+    try writer.writeAll("\n");
+    try bench.run(writer);
 }
--- src/root.zig
+++ src/root.zig
@@ -1,1 +1,121 @@
 const std = @import("std");
+
+pub const FastEncoder = encoder(u32, u8, u16);
+pub const TinyEncoder = encoder(u64, u8, u16);
+
+fn LookupTable(comptime Key: type, comptime Value: type) type {
+    return struct {
+        const Self = @This();
+        const SIZE = std.math.maxInt(Key) + 1;
+
+        table: []Value,
+
+        pub fn init(allocator: std.mem.Allocator) !Self {
+            const table = try allocator.alloc(Value, SIZE);
+            @memset(table, 0);
+            return Self{ .table = table };
+        }
+
+        pub inline fn deinit(self: *Self, allocator: std.mem.Allocator) void {
+            allocator.free(self.table);
+        }
+
+        pub inline fn get(self: *const Self, key: Key) Value {
+            return self.table[key];
+        }
+
+        pub inline fn set(self: *Self, key: Key, value: Value) void {
+            self.table[key] = value;
+        }
+    };
+}
+
+fn NumberHasher(comptime Data: type, comptime Hash: type) type {
+    return struct {
+        const PRIME = switch (Data) {
+            u128 => 0x9E3779B97F4A7C15F39CC0605CEDC7FD,
+            u64 => 0x9E3779B97F4A7C15,
+            u32 => 0x9D6EF916,
+            u16 => 0x9E3B,
+            u8 => 0x9D,
+            else => @compileError("Unsupported Data type size for Hasher"),
+        };
+        const SHIFT = @bitSizeOf(Data) - @bitSizeOf(Hash);
+
+        pub inline fn hash(data: Data) Hash {
+            return @truncate((data *% PRIME) >> SHIFT);
+        }
+    };
+}
+
+pub fn encoder(comptime Word: type, comptime Header: type, comptime Hash: type) type {
+    const Hasher = NumberHasher(Word, Hash);
+    const Table = LookupTable(Hash, Word);
+
+    const HEADER_BITS = @bitSizeOf(Header);
+    const WORD_BYTES = @sizeOf(Word);
+    const HEADER_BYTES = @sizeOf(Header);
+    const HASH_BYTES = @sizeOf(Hash);
+    const BATCH_BYTES = HEADER_BITS * WORD_BYTES;
+
+    return struct {
+        const Self = @This();
+        table: Table,
+
+        pub fn init(allocator: std.mem.Allocator) !Self {
+            return .{ .table = try Table.init(allocator) };
+        }
+
+        pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
+            self.table.deinit(allocator);
+        }
+
+        pub inline fn outputBufferBound(len: usize) usize {
+            const blocks = len / BATCH_BYTES;
+            return len + (blocks * HEADER_BYTES) + HEADER_BYTES + WORD_BYTES;
+        }
+
+        pub fn compressBlockToBuffer(self: *Self, input: []const u8, output: []u8) usize {
+            std.debug.assert(output.len >= outputBufferBound(input.len));
+
+            var input_index: usize = 0;
+            var output_index: usize = 0;
+            const loop_limit = (input.len / BATCH_BYTES) * BATCH_BYTES;
+
+            @setRuntimeSafety(false);
+
+            while (input_index < loop_limit) {
+                const header_pos = output_index;
+                output_index += HEADER_BYTES;
+                var header: Header = 0;
+
+                inline for (0..HEADER_BITS) |token_index| {
+                    const word: Word = std.mem.readInt(Word, input[input_index..][0..WORD_BYTES], .little);
+                    input_index += WORD_BYTES;
+
+                    const hash = Hasher.hash(word);
+                    const reference_word = self.table.get(hash);
+
+                    if (word == reference_word) {
+                        std.mem.writeInt(Hash, output[output_index..][0..HASH_BYTES], hash, .little);
+                        output_index += HASH_BYTES;
+                        header |= 1 << token_index;
+                    } else {
+                        std.mem.writeInt(Word, output[output_index..][0..WORD_BYTES], word, .little);
+                        output_index += WORD_BYTES;
+                        self.table.set(hash, word);
+                    }
+                }
+
+                std.mem.writeInt(Header, output[header_pos..][0..HEADER_BYTES], header, .little);
+            }
+
+            const remaining = input.len - input_index;
+            if (remaining != 0) {
+                @memcpy(output[output_index .. output_index + remaining], input[input_index .. input_index + remaining]);
+            }
+
+            return output_index + remaining;
+        }
+    };
+}
